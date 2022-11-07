929ab11b4bb8a044ac0490767f56ce40e879dd4f
Prometheus2k <shonesk2000@gmail.com>
2022-11-07 21:24:11 +0530

    Optimized images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 export default function Home({ data }) {
-  // console.log(data)
-  // const { title, description } = data.site.siteMetadata
+  console.log(data)
   return (
     <Layout>
       <section className={styles.header}>
@@ -17,22 +17,25 @@ export default function Home({ data }) {
             My Portfolio Projects
           </Link>
         </div>
-        <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
-        {/* <p>
-          {title} - {description}
-        </p> */}
+        <GatsbyImage
+          image={data.file.childImageSharp.gatsbyImageData}
+          alt="site banner"
+        />
       </section>
     </Layout>
   )
 }
 
-// export const query = graphql`
-//   query siteInfo {
-//     site {
-//       siteMetadata {
-//         description
-//         title
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query Banner {
+    file(relativePath: { eq: "banner.png" }) {
+      childImageSharp {
+        gatsbyImageData(
+          layout: FULL_WIDTH
+          placeholder: BLURRED
+          formats: [AUTO, WEBP]
+        )
+      }
+    }
+  }
+`
