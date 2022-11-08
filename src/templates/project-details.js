0547d0a1add66dc882ee24f68a0f2aa4e6c833d2
Prometheus2k <shonesk2000@gmail.com>
2022-11-08 12:46:14 +0530

    generating pages

---
+++ src/templates/project-details.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import * as styles from "../styles/project-details.module.css"
+
+export default function ProjectDetails() {
+  return (
+    <Layout>
+      <div className={styles.details}>
+        <h2>title</h2>
+        <h3>stack</h3>
+        <div className={styles.featured}>
+          {/* <GatsbyImage
+          image={data.file.childImageSharp.gatsbyImageData}
+          alt="site banner"
+        /> */}
+        </div>
+        {/* <div className={styles.html} dangerouslySetInnerHTML={}/> */}
+      </div>
+    </Layout>
+  )
+}
