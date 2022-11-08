8c150b709070a88bf38d860a80e34970f56da3e4
Prometheus2k <shonesk2000@gmail.com>
2022-11-08 11:09:24 +0530

    featured images

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.css"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 export default function Projects({ data }) {
   console.log(data)
@@ -16,6 +17,10 @@ export default function Projects({ data }) {
           {projects.map(project => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div>
+                <GatsbyImage
+                  image={getImage(project.frontmatter.thumb)}
+                  alt={project.frontmatter.slug}
+                />
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
@@ -38,9 +43,17 @@ export const query = graphql`
       nodes {
         frontmatter {
           slug
-          stack
           title
-          date
+          stack
+          thumb {
+            childImageSharp {
+              gatsbyImageData(
+                layout: FULL_WIDTH
+                placeholder: BLURRED
+                formats: [AUTO, WEBP]
+              )
+            }
+          }
         }
         id
       }
